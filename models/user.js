f39742298013f7710706711b1ff57608b9b61a2f
enkhnairamdal <enkhnairamdalbatmagnai@gmail.com>
2024-04-11 17:43:13 +0800

    phone

--- models/user.js
+++ models/user.js
@@ -6,8 +6,10 @@ const passwordComplexity = require("joi-password-complexity");
 const userSchema = new mongoose.Schema({
 	firstName: { type: String, required: true },
 	lastName: { type: String, required: true },
+	phone: { type: String, required: true },
 	email: { type: String, required: true },
 	password: { type: String, required: true },
+	
 });
 
 userSchema.methods.generateAuthToken = function () {
@@ -23,6 +25,7 @@ const validate = (data) => {
 	const schema = Joi.object({
 		firstName: Joi.string().required().label("First Name"),
 		lastName: Joi.string().required().label("Last Name"),
+		phone: Joi.string().required().label("Phone"),
 		email: Joi.string().email().required().label("Email"),
 		password: passwordComplexity().required().label("Password"),
 	});
